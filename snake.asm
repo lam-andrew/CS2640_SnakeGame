3ef70fb3b827b19ea632a4edce50b70849e48367
Andrew Lam <79387122+lam-andrew@users.noreply.github.com>
2022-11-25 18:16:24 -0800

    best practices

--- snake.asm
+++ snake.asm
@@ -1,4 +1,4 @@
 # Assignment: MIPS Assembly Final Project 
-# Course: CS2640.01: 
+# Course: CS2640.01
 # Group: Andrew Lam, Tyler Hipolito, Ben Le, James Contreras 
-# Objective: 
+# Objective: Create a game of snake in MIPS assembly langauge. Utilize all that we have learned in CS2640. 
